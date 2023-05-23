22c187591f9db461147f3e1de989e69f174e5245
rodrigo <ro-ag@github.com>
2023-05-23 00:38:55 -0700

    add memory information

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # yokai
+
+to update python modules:
+
+```shell
+cd daemon
+pip freeze > requirements.txt
+pip3 install -r requirements.txt
+cd ..
+```
--- daemon/executor.py
+++ daemon/executor.py
@@ -1,20 +1,38 @@
 import zmq
 import subprocess
 import os
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("ipc:///tmp/daemon.ipc")  # IPC socket
-s_pid = str(os.getpid())
-print("running daemon from pid: " + s_pid)
+import psutil
 
+# Create a ZeroMQ context
+context = zmq.Context()
+
+# Create a REP (reply) socket
+socket = context.socket(zmq.REP)
+
+# Bind the socket to an IPC endpoint
+socket.bind("ipc:///tmp/daemon.ipc")
+
+# Print the PID of this script for debugging purposes
+print("running daemon from pid: ", os.getpid())
+
+# Main loop
 while True:
-    #  Wait for next request from client
+    # Wait for next request from client
     script_path = socket.recv_string()
     print("Received request to run: ", script_path)
 
     # Execute the script and get the return code
-    process = subprocess.run(script_path, shell=True)
-    return_code = process.returncode
+    try:
+        process = subprocess.run(script_path, shell=True, timeout=5)
+        return_code = process.returncode
+    except subprocess.TimeoutExpired:
+        print("Script took too long to run.")
+        return_code = 1  # Indicate an error
 
-    #  Send reply back to client
+    # Get memory usage
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    print("Memory Info:", memory_info)
+
+    # Send reply back to client
     socket.send_string(str(return_code))
